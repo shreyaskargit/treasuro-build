e422e94b28c46f20d2f9f2720c2c107a10184e24
shreyaskargit <shreyaskar30@gmail.com>
2020-02-20 02:30:59 +0530

    bonus page

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.14a01f91b4cbefcf7486092864f4fb85.js"
+  "/precache-manifest.dcfa306b7344212a622bc13a9d0e544b.js"
 );
 
 self.addEventListener('message', (event) => {
